29b3d52b753e4bf9b62df635984ce1c5c52fe0c6
Carlos V <ae.c.vera@gmail.com>
2023-05-12 00:26:20 +0200

    debugging

--- scripts/script.js
+++ scripts/script.js
@@ -827,9 +827,9 @@ function ReseteoJuicios(){
 function shouldShowFifthQuestion() {
     // Replace this with the actual condition
     if ((FaseTest.secuenciaResps.reduce((a, b) => a + b, 0) / FaseTest.numTrials) > 0.94 || (FaseTest.secuenciaResps.reduce((a, b) => a + b, 0) / FaseTest.numTrials) < 0.5) {
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
 
 function saveAnswer(){
@@ -875,22 +875,30 @@ function siguienteTexto(){
 				console.log("Estoy guardando la respuesta en testeo - " + dataToSave);
 			}        
 		}
-		if (stateTexto >= arrayInstruc.length - 7 && stateTexto < arrayInstruc.length - 3) {
+		if (stateTexto >= arrayInstruc.length - 7 && stateTexto < arrayInstruc.length - 2 - shouldShowFifthQuestion()) {
 	    	htmlContenido += `<br><textarea id="questionText${stateTexto}" rows="10" cols="50" style="width: 100%;" oninput="saveAnswer(${stateTexto})"></textarea>`;
 		}
 	}
-    if (stateTexto == arrayInstruc.length - 3) {
+//    if (stateTexto == arrayInstruc.length - 3) {
 		
-		if (shouldShowFifthQuestion()) {
+//		if (shouldShowFifthQuestion()) {
         // Show the 5th question
-			console.log("Mostrando la pregunta 5");
-			console.log(stateTexto + " ## Se va a guardard como id=" + "questionText$" + stateTexto);
-			htmlContenido += `<br><textarea id="questionText${stateTexto}" rows="10" cols="50" style="width: 100%;" oninput="saveAnswer(${stateTexto})"></textarea>`;
-		}
-		else { // En este caso no hay que mostrar la pregunta 5 porque no se cumple la condición
+//			console.log("Mostrando la pregunta 5");
+//			console.log(stateTexto + " ## Se va a guardard como id=" + "questionText$" + stateTexto);
+//			htmlContenido += `<br><textarea id="questionText${stateTexto}" rows="10" cols="50" style="width: 100%;" oninput="saveAnswer(${stateTexto})"></textarea>`;
+//		}
+//		else { // En este caso no hay que mostrar la pregunta 5 porque no se cumple la condición
+//			stateTexto++;
+//		}
+
+//    if (stateTexto == arrayInstruc.length - 3) {
+		
+		if (!shouldShowFifthQuestion()) {
 			stateTexto++;
 		}
-        htmlContenido = "Your fifth question text:<br>";
+
+
+		htmlContenido = "Your fifth question text:<br>";
         htmlContenido += `<br><textarea id="questionText5" rows="10" cols="50" style="width: 100%;" oninput="saveAnswer(5)"></textarea>`;
     }
 	
